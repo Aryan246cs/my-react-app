82197f20b194976d8b3f1b2f8dd979a6218204d3
Aryan246cs <aryansrivastava24062005@gmail.com>
2025-08-30 15:25:21 +0530

    till rendering list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
 function App() {
+
+  const fruits = [{id:1,name:"apple",calories: 95},
+                    {id:2,name:"orange",calories:45},
+                    {id:3,name:"mango",calories: 205},
+                    {id:4,name:"banana",calories:105},
+                    {id:5,name:"papaya",calories:83}];
+    
+  const vegetables = [{id:6,name:"potatoes",calories: 110},
+                    {id:7,name:"celery",calories:15},
+                    {id:8,name:"carrots",calories: 25},
+                    {id:9,name:"corn",calories:63},
+                    {id:10,name:"broccoli",calories:50}];
+    
   return(
     <>
-     <Button />
+    <List items={fruits} category="Fruits"/>
+    <List items ={vegetables} category="Vegetables" />
     </>
    
   );
---
+++ src/List.jsx
@@ -0,0 +1,23 @@
+function List(props) {
+
+    const itemList = props.items;
+    const category = props.category;
+
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); // alphabetical
+    //fruits.sort({a,b} => b.name.localCompare(a.name)); //reverse alphabetical
+    //fruits.sort((a,b) => a.calories -b.calories);// numeric
+    // fruits.sort((a,b) => b.calories -a.calories); // reverse numeric
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+    const listItems = itemList.map(item => <li key={item.id}>
+        {item.name} : &nbsp;
+        <b>{item.calories}</b></li>);
+
+
+    return (<>
+        <h3 className="list-category">{category}</h3>
+        <ol className="list-items">{listItems}</ol>
+    </>);
+}
+export default List
